3b4ab22418e2bb2388e147a64c20cce775196982
trymboe <trym.boe@gmail.com>
2023-03-19 17:39:40 +0100

    added cifar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import ssl
 import torch
+import pickle
 import hashlib
 import statistics
 import collections
@@ -8,16 +10,20 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 
 
+
 from PIL import Image
 from torch.utils.data import DataLoader
 from sklearn.metrics import precision_score
 from sklearn.model_selection import train_test_split
+from torchvision.models import resnet50, ResNet50_Weights
 
 from resnet import ResNet18
 from dataloader import Image_DL
 
 np.random.seed(42)
 torch.manual_seed(42)
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 CLASSES = {
             "forest" : [1,0,0,0,0,0], "buildings" : [0,1,0,0,0,0],
@@ -96,11 +102,11 @@ def process_data(path):
     images = []
     labels = []
     hashes = set()
-
+    print("-- loading images --")
     for folder in os.listdir(path):
         #dont want .DS_Store
-        if "DS" not in folder:
-            for img in os.listdir(path+'/'+folder):
+        if "cifar" not in folder and "DS" not in folder:
+            for img in os.listdir(path+'/'+folder+'/test'):
                 if img.endswith('.jpg'):
                     image = np.array(Image.open(os.path.join(path,folder,img)))
                     image_hash = get_image_hash(image)
@@ -268,32 +274,79 @@ def train_model(model, criterion, optimizer, model_path, device):
 
     torch.save(model.state_dict(), model_path)
 
+def find_indices(list_to_check, item_to_find):
+    indices = []
+    for idx, value in enumerate(list_to_check):
+        if value == item_to_find:
+            indices.append(idx)
+    return indices
 
 def get_top_bottom_10(softmax, inputs, labels):
+
+    class_0 = (find_indices(list(labels), 0))
+    class_1 = (find_indices(list(labels), 1))
+    class_2 = (find_indices(list(labels), 2))
+    class_0_images = [inputs[i] for i in class_0]
+    class_1_images = [inputs[i] for i in class_0]
+    class_2_images = [inputs[i] for i in class_0]
+
+    class_0_softmax = [softmax[i] for i in class_0]
+
+
     # Get the indices of the highest and lowest probability scores
-    max_values = torch.topk(softmax, k=1, dim=1)[0].squeeze(1)
-    top_values, top_indices = torch.topk(max_values, k=10)
+    max_values = list(torch.topk(softmax, k=1, dim=1)[0].squeeze(1))
+
+    class_0_max_values = torch.FloatTensor([max_values[i] for i in class_0])
+    class_1_max_values = torch.FloatTensor([max_values[i] for i in class_1])
+    class_2_max_values = torch.FloatTensor([max_values[i] for i in class_2])
+    top_indices_0 = torch.topk(class_0_max_values, k=10)[1]
+    top_indices_1 = torch.topk(class_1_max_values, k=10)[1]
+    top_indices_2 = torch.topk(class_2_max_values, k=10)[1]
     
     min_values = torch.topk(softmax, k=1, dim=1,largest=False)[0].squeeze(1)
-    bottom_values, bottom_indices = torch.topk(min_values, k=10, largest=False)
+    class_0_min_values = torch.FloatTensor([min_values[i] for i in class_0])
+    class_1_min_values = torch.FloatTensor([min_values[i] for i in class_1])
+    class_2_min_values = torch.FloatTensor([min_values[i] for i in class_2])
+    bottom_indices_0 = torch.topk(class_0_min_values, k=10)[1]
+    bottom_indices_1 = torch.topk(class_1_min_values, k=10)[1]
+    bottom_indices_2 = torch.topk(class_2_min_values, k=10)[1]
 
     for i in range(10):
+        image = np.transpose(class_0_images[top_indices_0[i]].numpy(), (2, 1, 0))
+        plt.imshow(image)
+        plt.title(f"Top score: label: forest, predicted: {CLASSES_LIST[torch.topk(softmax[top_indices_0[0]], k=1)[1].item()]}")
+        plt.figure()
 
-        image = np.transpose(inputs[top_indices[i]].numpy(), (2, 1, 0))
-        plt.imshow(image)
-        plt.title(f"Top score: label: {CLASSES_LIST[labels[top_indices[i]]]}, predicted: {CLASSES_LIST[torch.topk(softmax[top_indices[0]], k=1)[1].item()]}")
-        plt.figure()
-        
-        image = np.transpose(inputs[bottom_indices[i]].numpy(), (2, 1, 0))
-        plt.imshow(image)
-        plt.title(f"Bottom score: label: {CLASSES_LIST[labels[bottom_indices[i]]]}, predicted: {CLASSES_LIST[torch.topk(softmax[top_indices[0]], k=1)[1].item()]}")
-        plt.figure()
+    # for i in range(10):
+    #     image = np.transpose(class_0_images[bottom_indices_0[i]].numpy(), (2, 1, 0))
+    #     plt.imshow(image)
+    #     plt.title(f"Bottom score: label: forest, predicted: {CLASSES_LIST[torch.topk(softmax[bottom_indices_0[0]], k=1)[1].item()]}")
+    #     plt.figure()
+    
+    # for i in range(10):
+    #     image = np.transpose(class_1_images[top_indices_1[i]].numpy(), (2, 1, 0))
+    #     plt.imshow(image)
+    #     plt.title(f"Top score: label: buildings, predicted: {CLASSES_LIST[torch.topk(softmax[top_indices_1[0]], k=1)[1].item()]}")
+    #     plt.figure()
+    # for i in range(10):
+    #     image = np.transpose(class_1_images[bottom_indices_1[i]].numpy(), (2, 1, 0))
+    #     plt.imshow(image)
+    #     plt.title(f"Bottom score: label: buildings, predicted: {CLASSES_LIST[torch.topk(softmax[bottom_indices_1[0]], k=1)[1].item()]}")
+    #     plt.figure()
+    
+    # for i in range(10):
+    #     image = np.transpose(class_2_images[top_indices_2[i]].numpy(), (2, 1, 0))
+    #     plt.imshow(image)
+    #     plt.title(f"Top score: label: glacier, predicted: {CLASSES_LIST[torch.topk(softmax[top_indices_2[0]], k=1)[1].item()]}")
+    #     plt.figure()
+    # for i in range(10):
+    #     image = np.transpose(class_2_images[bottom_indices_2[i]].numpy(), (2, 1, 0))
+    #     plt.imshow(image)
+    #     plt.title(f"Bottom score: label: glacier, predicted: {CLASSES_LIST[torch.topk(softmax[bottom_indices_2[0]], k=1)[1].item()]}")
+    #     plt.figure()
 
     plt.show()
     
-
-
-
 def evaluate(model, criterion, dataloader, device, per_class=True, show=False, test=False):
     '''
     Evaluates a dataset on a model. Is used for validation for validation set and test set.
@@ -353,7 +406,7 @@ def evaluate(model, criterion, dataloader, device, per_class=True, show=False, t
                 correct_counts[i] += torch.sum(mask4 == True,dim=0)
                 
             if test:
-                get_top_bottom_10(outputs, inputs, labels)
+                get_top_bottom_10(outputs, inputs, labels, 0)
                 torch.save(outputs, 'test_softmax.pt')
                 torch.save(labels, 'test_labels.pt')
 
@@ -386,29 +439,113 @@ def evaluate(model, criterion, dataloader, device, per_class=True, show=False, t
 
     return val_loss, accuracy, class_wise_acc, class_wise_precision
 
+def get_statistics(model, device, dataloader, task3):
+    all_modules = []
+    for nam, mod in model.named_modules():
+        if "conv" in nam:
+            all_modules.append(mod)
+
+    #I use the 2 first and 3 last moduelsed with conv in the name
+    first_modules = all_modules[:2]
+    last_modules = all_modules[-3:]
+    selected_modules = first_modules + last_modules
+
+
+    hooks = []
+    for module in selected_modules:
+        if task3:
+            hook = module.register_forward_hook(forward_hook1)
+        else:
+            hook = module.register_forward_hook(forward_hook2)    
+        hooks.append(hook)
+  
+    if task3:
+        resnet_model = resnet50(weights = ResNet50_Weights.IMAGENET1K_V2)
+        datasets = [dataloader]
+        cifar = unpickle("data/cifar-10-batches-py/test_batch")
+
+
+
+
+    else:
+        # Iterate over your dataloader and compute the average non-positive percentage for each feature map
+        count = 0
+        total_non_pos_percentages = [0] * 5
+        for inputs, targets in dataloader:
+            outputs = model(inputs.to(device))
+            count += 1
+            if count == 200:
+                break
+
+    # Remove the forward hooks
+    for hook in hooks:
+        hook.remove()
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+
+def forward_hook1(module, input, output):
+    non_pos_count = torch.sum(output <= 0)
+    total_count = output.numel()
+    non_pos_percentage = non_pos_count.float() / total_count * 100
+    avg_non_pos_percentage = non_pos_percentage / 200
+    print(f"Average non-positive percentage in {module} output: {avg_non_pos_percentage:.2f}%")
+
+def forward_hook2(module, input, output):
+    k = 1000
+    feature_map = output.detach()
+    spatial_mean = torch.mean(feature_map, dim=[2, 3], keepdim=True)
+    # Reshape the mean feature map tensor to (batch_size, num_channels), assuming num_channels is the lastdimention
+    mean_feature_map = spatial_mean.view(spatial_mean.size(0), -1)
+    covariance_matrix = torch.cov(mean_feature_map)
+
+    # Compute the top-k eigenvalues and sort them in decreasing order
+    eigenvalues, _ = torch.symeig(covariance_matrix, eigenvectors=False)
+    top_k_eigenvalues, _ = torch.sort(eigenvalues, descending=True)[:k]
+
+    plt.bar(range(len(top_k_eigenvalues)), top_k_eigenvalues)
+    plt.xlabel('Eigenvalue index')
+    plt.ylabel('Eigenvalue magnitude')
+    plt.title('Top-k eigenvalues of empirical covariance matrix')
+    plt.show()
+
+
+
 
 if __name__ == "__main__":
-    print("start")
     # Set device to use
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     #for training on GPU for M1 mac
     #device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
-    model_path = "models/model1_e45_lr0.001_2"
-    train = True
+    model_path = "models/model1_e45_lr0.001"
+    train = False
+    hooks = True
+    task3 = True
+
+    
 
     train_loader, test_loader, val_loader = process_data("data")
-    print("data loaded")
+    print("-- data loaded --")
     model, criterion, optimizer = create_model(device)
-    print("model created")
+    print("-- model created --")
     if train:
+        print("-- training model --")
         train_model(model, criterion, optimizer, model_path, device)
     else:
+        print("-- loading model --")
         model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
         model.eval()
-        print("\n\nTest set evaluation")
-        loss, accuracy, class_wise_acc, class_wise_precision = evaluate(model, criterion, test_loader, device, show=False, test=True)
-        plt.show()
+        if hooks:
+            print("-- calculating statistics --")
+            get_statistics(model, device, test_loader, task3)
+        else:
+            print("\n\n-- Test set evaluation --")
+            loss, accuracy, class_wise_acc, class_wise_precision = evaluate(model, criterion, test_loader, device, show=False, test=True)
+
 
     
 
