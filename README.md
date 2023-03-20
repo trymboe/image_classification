08ceadc75d4d681acd937b52a7f7e47c2c598582
trymboe <trym.boe@gmail.com>
2023-03-20 21:55:20 +0100

    added readme and argsparse

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# image_classification
+# Image_classification
+
+This repo contains the python code to train and run an image classification network.
+Test data labels, and test data softmax are available in the root folder as .pt files.
+
+
+## usage
+
+### argparse
+image_classification.py [-h] [-t] [--model_path MODEL_PATH] [-td] [--task2] [--task3]
+
+Image classification network
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t, --train           train the model (default: False)
+  --model_path MODEL_PATH
+                        path to save the trained model (default: None)
+  -td, --test_data      use a small dataset for testing (default: False)
+  --task2               run task 2 (default: False)
+  --task3               run task 3 (default: False)
+
+
+## running 
+Install required packages using 
+
+```python
+pip3 install -r requirements.txt
+```
+
+To run this program, run main.py.
+
+If [-t, --train] is not active, the evaluation parth will start using a pretrained model that is found in the models directory.
+If you want to train your own model, you can add the [-t, --train] argument. This also requires [--model_path] to be added, and 
+is the path the newly trained model will be saved to.
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import ssl
 import torch
 import argparse
+import warnings
 import numpy as np
 
 from model import create_model
@@ -13,6 +14,7 @@ np.random.seed(42)
 torch.manual_seed(42)
 
 ssl._create_default_https_context = ssl._create_unverified_context
+warnings.filterwarnings('always')
 
 CLASSES_LIST = ["forest", "buildings",
             "glacier", "street",
@@ -21,17 +23,28 @@ CLASSES_LIST = ["forest", "buildings",
 BATCH_SIZE = 128
 LR = 0.001
 MOMENTUM = 0.9
-EPOCHS = 5
+EPOCHS = 45
 
 
 
 
 if __name__ == "__main__":
-    train = False
-    hooks = True
-    task3 = True
+    parser = argparse.ArgumentParser(description='Image classification network')
+    parser.add_argument('-t','--train', action='store_true')
+    parser.add_argument('--model_path')
+
+    parser.add_argument('-td','--test_data', action='store_true')
+    parser.add_argument('--task2', action='store_true')
+    parser.add_argument('--task3', action='store_true')
+
+
+    args = vars(parser.parse_args())
+
+    train = args['train']
+    hooks = args['task2']
+    task3 = args['task3']
     mac = False
-    testdata = True
+    testdata = args['test_data']
 
     if mac:
         #for training on GPU for M1 mac
@@ -40,7 +53,10 @@ if __name__ == "__main__":
         # Set device to use
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model_path = "models/model1_e45_lr0.001"
+    if train and not args['model_path']:
+        print("if you want to train, you need to add a model path --model_path")
+        exit()
+    model_path = "models/model1_e50_lr0.001" if not train else args['model_path']
 
     train_loader, test_loader, val_loader = process_data("data", BATCH_SIZE, testdata=testdata)
     print("-- data loaded --")
@@ -53,7 +69,7 @@ if __name__ == "__main__":
         print("-- loading model --")
         model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
         model.eval()
-        if hooks:
+        if hooks or task3:
             print("-- calculating statistics --")
             get_statistics(model, device, task3)
         else:
